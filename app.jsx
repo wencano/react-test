04602a205363a10528672d25b7ab66c3529ea307
wencano <wen@wencano.com>
2020-02-07 16:07:29 +0800

    example pages

--- app.jsx
+++ app.jsx
@@ -1,4 +1,72 @@
+
+
+
+class App extends React.Component {
+  
+  /**
+   * Constructor
+   */
+  constructor(props) {
+    super(props);
+    this.state = {
+      page: ''
+    }
+
+    this.changePage = this.changePage.bind(this);
+
+  }
+
+
+  /**
+   * Change Page
+   */
+  changePage(page) {
+    this.setState({...this.state,  page: page });
+  }
+
+
+  /**
+   * Render Page
+   */
+  render() {
+    return (
+      <div>
+        <Nav data={this.state} changePage={this.changePage} />
+        
+        {this.state.page == '' || this.state.page == 'dashboard' ? 
+          <Dashboard />
+          : null
+        }
+
+        {this.state.page == 'users' ? 
+            <main role="main">
+              <div className="container">
+                <h1>Users page</h1>
+              </div>
+            </main>
+          : null
+        }
+
+        {this.state.page == 'settings' ? 
+            <main role="main">
+              <div className="container">
+                <h1>Settings page</h1>
+              </div>
+            </main>
+          : null
+        }   
+
+
+      </div>
+    )
+  }
+}
+
+
+/**
+ * Mount component to #root
+ */
 ReactDOM.render(
-  <Clicker />,
+  <App />,
   document.getElementById('root')
 );
